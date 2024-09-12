62f986c68b3e269fd57ec24cb56ccfeb325433a8
Sylvain-pitie <sylvain194@outlook.fr>
2024-09-12 11:55:35 +0200

    Update band1.py

--- band1.py
+++ band1.py
@@ -175,7 +175,7 @@ def band1(kindatm,atm1,typeorb1,color1,title,labelfig,xanch,yanch,fsize,xrot,emi
     plt.legend(loc="upper right",fontsize=fsize,bbox_to_anchor=(xanch, yanch),frameon=False)
     plt.xticks(fontsize=fsize)
     plt.yticks(fontsize=fsize)
-    fig.text(0.01, 0.96, labelfig, fontsize=22)
-    plt.subplots_adjust(top=0.96, bottom=0.11, left=0.12, right=0.98,wspace=0.15)
+    fig.text(0.01, 0.95, labelfig, fontsize=22)
+    plt.subplots_adjust(top=0.93, bottom=0.11, left=0.12, right=0.98,wspace=0.15)
 
     plt.savefig(atm1+"bandplot.png", format="png", dpi=dpi)
